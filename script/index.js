51d57396aa3ba787635a567e60116d9928837683
jcy981215 <jcy981215@naver.com>
2023-10-18 00:49:46 +0900

    서브메뉴 완료

--- script/index.js
+++ script/index.js
@@ -1,7 +1,24 @@
-$('header .nav > li > .sub').hide()
 $('header .nav').on('mouseover',function(){
-    $('header .nav > li > .sub').stop().slideDown()
+    $('header .sub_bg ').stop().slideDown()
+    $('header .sub_bg ').css('backgroundColor', '#000')
+    // $('header').css('backgroundColor', '#000')
 })
-$('header .nav').on('mouseout',function(){
-    $('header .nav > li > .sub').stop().slideUp()
+$('main').on('mouseover',function(){
+    $('header .sub_bg').stop().slideUp()
+    $('header').css('backgroundColor', 'rgba(0, 0, 0, 0.40)')
+})
+$('header .nav #main_nav1').on('mouseover',function(){
+    $('header .sub_bg #sub1').stop().slideDown()
+    $('header .sub_bg #sub2').hide()
+    $('header .sub_bg #sub3').hide()
+})
+$('header .nav #main_nav2').on('mouseover',function(){
+    $('header .sub_bg #sub1').hide()
+    $('header .sub_bg #sub2').stop().slideDown()
+    $('header .sub_bg #sub3').hide()
+})
+$('header .nav #main_nav3').on('mouseover',function(){
+    $('header .sub_bg #sub1').hide()
+    $('header .sub_bg #sub2').hide()
+    $('header .sub_bg #sub3').stop().slideDown()
 })
